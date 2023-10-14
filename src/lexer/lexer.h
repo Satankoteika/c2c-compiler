bf9894a9ee010904c0f4edf1fe49488622f030bb
Satankoteika <amen.sapocalypse@gmail.com>
2023-10-14 21:55:33 +0300

    Format and small fixes

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -5,78 +5,70 @@
 
 #include <vector>
 
-namespace ctc::lexer
-{
-    struct lexer_error
+namespace ctc::lexer {
+struct lexer_error {
+    std::string what;
+    size_t line, pos;
+};
+
+struct lexer_options {
+    bool skip_errors;
+};
+
+struct lexer_results {
+    std::vector<token> tokens;
+    std::vector<lexer_error> errors;
+
+    bool successfully { true };
+};
+
+class lexer {
+private:
+    lexer_options options;
+    std::string m_source;
+    size_t m_pos, m_line, m_suboffset;
+
+    // Helpful methods
+    inline char next()
     {
-        inline lexer_error(std::string&& error_what, size_t error_line, size_t error_pos) : {}
+        return m_source[m_pos++];
+    }
 
-        std::string what;
-
-        size_t line, pos;
-    };
-
-    struct lexer_options
+    inline bool match(char to_find)
     {
-        bool skip_errors;
-    };
+        if (is_end() || m_source.at(m_pos) != to_find)
+            return false;
 
-    struct lexer_results
+        return true;
+    }
+
+    inline char peek(size_t index) const
     {
-        std::vector<token> tokens;
-        std::vector<lexer_error> errors;
+        return (m_pos + index >= m_source.length()) ? '\0' : m_source.at(m_pos + index);
+    }
 
-        bool successfully {true};
-    };
-
-    class lexer
+    inline char peek_next() const
     {
-    private:
-        lexer_options options;
+        return (m_pos + 1 >= m_source.length()) ? '\0' : m_source.at(m_pos + 1);
+    }
 
-        std::string m_source;
+    inline bool is_end() const
+    {
+        return (m_pos >= m_source.length());
+    }
 
-        size_t m_pos, m_line, m_suboffset;
+    void tokenize_string_literal(lexer_results &results) noexcept;
+    void tokenize_number_literal();
+    void tokenize_identifier();
 
-        // Helpful methods
+public:
+    explicit inline lexer(lexer_options options_for_lexing)
+        : options(options_for_lexing)
+    {
+    }
 
-        inline char next()
-        {
-            return m_source[m_pos++];
-        }
-
-        inline bool match(char to_find)
-        {
-            if (is_end() || m_source.at(m_pos) != to_find)
-                return false;
-
-            return true;
-        }
-
-        inline char peek(size_t index) const
-        {
-            return (m_pos + index >= m_source.length()) ? '\0' : m_source.at(m_pos + index);
-        }
-
-        inline char peek_next() const
-        {
-            return (m_pos + 1 >= m_source.length()) ? '\0' : m_source.at(m_pos + 1);
-        }
-
-        inline bool is_end() const
-        {
-            return (m_pos >= m_source.length());
-        }
-
-        void tokenize_string_literal(lexer_results& results) noexcept;
-        void tokenize_number_literal();
-        void tokenize_identifier();
-
-    public:
-        explicit inline lexer(lexer_options options_for_lexing) : options(options_for_lexing) {}
-
-        lexer_results tokenize_from_source(std::string&& source);
-    };
+    lexer_results tokenize_from_source(std::string &&source);
+};
 }
 
 #endif
