13b720fbcef584636be26a45644dcda49f8d16d8
Satankoteika <amen.sapocalypse@gmail.com>
2023-10-19 00:14:51 +0300

    Premerge safety commit

--- src/preprocessor/pp_macroprocessor.h
+++ src/preprocessor/pp_macroprocessor.h
@@ -18,8 +18,28 @@ struct pp_macro {
 
 class pp_macroprocessor {
     std::vector<pp_token> m_tokens;
+    size_t m_pos;
 
-    void make_macro(size_t index, std::unordered_map<std::string, pp_macro> &macros);
+    inline const pp_token &next()
+    {
+        m_pos++;
+        if (m_pos >= m_tokens.size())
+            return m_tokens[m_tokens.size() - 1];
+        return m_tokens[m_pos];
+    }
+
+    inline const pp_token &peek(size_t index) const
+    {
+        return (m_pos + index >= m_tokens.size()) ? m_tokens[m_tokens.size() - 1]
+                                                  : m_tokens[m_pos + index];
+    }
+
+    inline const pp_token &peek_next() const
+    {
+        return (m_pos + 1 >= m_tokens.size()) ? m_tokens[m_tokens.size() - 1] : m_tokens[m_pos + 1];
+    }
+
+    bool is_end() { return m_pos + 1 >= m_tokens.size(); }
 
 public:
     pp_macroprocessor_results process_tokens(std::vector<pp_token> &&tokens);
